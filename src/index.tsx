c54d090424a663a1274e51a9c1f613f7e8ce8860
Nick Iaconis <nick.iaconis+github@gmail.com>
2019-03-09 13:32:26 -0800

    add tslint
    [x] React
    [x] Typescript
    [x] React-Snap (SEO)
    [ ] CSS Blocks
    [x] Favicon
    [ ] Jest
    [x] Tslint
    [ ] Prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { hydrate, render } from 'react-dom';
-import App from './App';
+import React from "react";
+import { hydrate, render } from "react-dom";
+import App from "./App";
 
-import '../public/android-chrome-192x192.png';
-import '../public/android-chrome-512x512.png';
-import '../public/apple-touch-icon.png';
-import '../public/browserconfig.xml';
-import '../public/favicon-16x16.png';
-import '../public/favicon-32x32.png';
-import '../public/mstile-150x150.png';
-import '../public/safari-pinned-tab.svg';
-import '../public/site.webmanifest';
+import "../public/android-chrome-192x192.png";
+import "../public/android-chrome-512x512.png";
+import "../public/apple-touch-icon.png";
+import "../public/browserconfig.xml";
+import "../public/favicon-16x16.png";
+import "../public/favicon-32x32.png";
+import "../public/mstile-150x150.png";
+import "../public/safari-pinned-tab.svg";
+import "../public/site.webmanifest";
 
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
