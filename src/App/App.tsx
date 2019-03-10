1224555babc675dd3d15754ae4332bc44bf85c72
Nick Iaconis <nick.iaconis+github@gmail.com>
2019-03-10 13:22:15 -0700

    css-blocks setup issues

---
+++ src/App/App.tsx
@@ -0,0 +1,10 @@
+import objstr from 'obj-str';
+import * as React from 'react';
+import styles from './App.block.css';
+
+export default function App() {
+  const style = objstr({
+    [styles]: true,
+  });
+  return <h1 class={style}>Hello world!</h1>;
+}
--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,13 +1,50 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CssBlocks = require('@css-blocks/jsx');
+const CssBlocksPlugin = require('@css-blocks/webpack').CssBlocksPlugin;
+
+if (process.env.NODE_ENV == null) {
+  process.env.NODE_ENV = 'development';
+}
+
+const entryPoint = './src/index.tsx';
+
+const jsxCompilationOptions = {
+  compilationOptions: {},
+  optimization: {
+    rewriteIdents: true,
+    mergeDeclarations: true,
+    removeUnusedStyles: true,
+    conflictResolution: true,
+    enabled: process.env.NODE_ENV === 'production',
+  },
+  aliases: {},
+};
+
+const CssBlockRewriter = new CssBlocks.Rewriter(jsxCompilationOptions);
+const CssBlockAnalyzer = new CssBlocks.Analyzer(
+  entryPoint,
+  jsxCompilationOptions
+);
 
 module.exports = {
   entry: {
-    app: './src/index.tsx',
+    app: entryPoint,
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new CssBlocksPlugin({
+      analyzer: CssBlockAnalyzer,
+      outputCssFile: 'blocks.css',
+      name: 'css-blocks',
+      compilationOptions: jsxCompilationOptions.compilationOptions,
+      optimization: jsxCompilationOptions.optimization,
+    }),
     new HtmlWebpackPlugin({
       title: 'Pawcapella 🐾🎶',
       filename: 'index.html',
@@ -18,32 +55,54 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/react',
-              '@babel/typescript',
-              [
-                '@babel/env',
-                {
-                  modules: false,
-                },
-              ],
-            ],
-          },
-        },
         exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/react',
+                '@babel/typescript',
+                [
+                  '@babel/env',
+                  {
+                    modules: false,
+                  },
+                ],
+              ],
+            },
+          },
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                require('@css-blocks/jsx/dist/src/transformer/babel').makePlugin(
+                  {
+                    rewriter: CssBlockRewriter,
+                  }
+                ),
+              ],
+              cacheDirectory: false,
+              compact: true,
+              parserOpts: {
+                plugins: ['jsx'],
+              },
+            },
+          },
+          {
+            loader: require.resolve('@css-blocks/webpack/dist/src/loader'),
+            options: {
+              analyzer: CssBlockAnalyzer,
+              rewriter: CssBlockRewriter,
+            },
+          },
+        ],
       },
       {
         test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$|\.webmanifest$|\.xml$/,
         loader: 'file-loader?name=[name].[ext]', // <-- retain original file name
       },
     ],
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
     extensions: ['.js', '.ts', '.tsx'],
